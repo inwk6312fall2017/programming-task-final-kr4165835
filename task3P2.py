19c00f8af52952d8e872d1634a33a0c54abf1da7
vijayabalaajitm <vijayb@dal.ca>
2017-10-20 15:48:18 -0300

    task3 part2

---
+++ task3P2.py
@@ -0,0 +1,15 @@
+file=open("running-config.cfg" ,'r')
+Acess_file=file.readline() 
+A=" "
+For word in Acessfile :
+  if word = "access": 
+   A.appand(Acess_file,'/n') 
+  else:
+   pass..
+dict={}
+for line in A:
+item=line.strip()
+dict("item[2:4]"=item(len[item])  
+
+print(dict)
+
